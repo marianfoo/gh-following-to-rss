a1a3f0051ed7c37f6646ef205410c9c8a05da25b
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-06-15 10:59:53 +0200

    Initial commit

---
+++ src/controller/App.controller.ts
@@ -0,0 +1,13 @@
+import BaseController from "./BaseController";
+
+/**
+ * @namespace de.marianzeis.githubfollower.controller
+ */
+export default class App extends BaseController {
+
+	public onInit() : void {
+		// apply content density mode to root view
+		this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+	}
+
+}
